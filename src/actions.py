2ed6da5961cd1bacd77a83eed7596bfc871e1f2e
Maximilian Schalk <maximilian.schalk@finn.auto>
2022-06-12 00:05:30 +0200

    full mvp

--- src/actions.py
+++ src/actions.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import re
 
@@ -6,51 +7,122 @@ import requests
 from src.api_interface.make_request import make_request_sync
 from src.STAGE import STAGE
 
-AUTHOR_STR = "GameHive #raid-seed-export#0000"
+BOT_AUTHOR = "pingu#4195"
+SEED_AUTHOR = "GameHive #raid-seed-export#0000"
 REGEX_CONTENT = re.compile("^Raid seed export - ([0-9]{4}/[0-9]{2}/[0-9]{2})$")
 
 EMOJI_CHECK_MARK = "✅"
+EMOJI_RED_CROSS = "❌"
+
+
+async def reaction_handled(reaction):
+    if reaction.emoji not in {EMOJI_CHECK_MARK, EMOJI_RED_CROSS}:
+        return False
+
+    if reaction.me:
+        return True
+
+    async for user in reaction.users():
+        if full_username(user) == BOT_AUTHOR:
+            return True
+
+    return False
 
 
 async def handle_existing_seed_messages(channel):
     async for msg in channel.history():
-        relevant = (
-            str(msg.author) == AUTHOR_STR
-            and REGEX_CONTENT.match(msg.content)
-        )
 
-        if not relevant:
+        for r in msg.reactions:
+            if await reaction_handled(r):
+                return
+
+        # handled = any([await reaction_handled(r) for r in msg.reactions])
+
+        # if handled:
+        #     return
+
+        try:
+            await handle_message(msg)
+        except RuntimeError as e:
+            await msg.add_reaction(emoji=EMOJI_RED_CROSS)
+            await msg.reply(e)
+
             continue
 
-        handled = any(map(
-            lambda reaction: reaction.me and reaction.emoji == EMOJI_CHECK_MARK, msg.reactions
-        ))
+            error_msg = f"Bot startup: Erroneous existing messages, error details as replies to {EMOJI_RED_CROSS} messages"
+            raise RuntimeError(error_msg)
 
-        if handled:
+
+async def handle_message(msg):
+    author_name = full_username(msg.author)
+
+    relevant = (
+        (author_name in {SEED_AUTHOR, BOT_AUTHOR})
+        and
+        REGEX_CONTENT.match(msg.content)
+    )
+
+    if not relevant:
+        return
+
+    print("relevant message found:", msg.content)
+
+    if len(msg.attachments) != 1:
+        await msg.add_reaction(emoji=EMOJI_RED_CROSS)
+
+        await throw_err_on_msg(
+            msg, f"Message fits criteria (author, content format), but has {len(msg.attachments)} attachments: {msg.attachments}"
+        )
+
+    a, *_ = msg.attachments
+
+    data = requests.get(a.url).json()
+
+    filename = build_filename(msg.content)
+
+    response = make_request_sync(
+        method=requests.post,
+        path=f"admin/raw_seed_file/{filename}",
+        data=json.dumps(data),
+        stage=STAGE,
+        parse_response=False
+    )
+
+    response_data = response.json()
+
+    if response.status_code == 200:
+
+        if response_data["file_created"]:
+            await msg.add_reaction(emoji=EMOJI_CHECK_MARK)
             return
 
-        print(msg.content)
-
-        if len(msg.attachments) != 1:
-            # Log warning
-            continue
-
-        a, *_ = msg.attachments
-
-        data = requests.get(a.url).json()
-
-        filename = build_filename(msg.content)
-
-        response = make_request_sync(
-            method=requests.post,
-            path=f"admin/raw_seed_file/{filename}",
-            data=json.dumps(data),
-            stage=STAGE,
-            parse_response=False
+        await throw_err_on_msg(
+            msg, f"File could not be created on server: {response_data}"
         )
 
-        if response.status_code == 200:
-            await msg.add_reaction(emoji=EMOJI_CHECK_MARK)
+    await throw_err_on_msg(
+        msg, f"Server returned status code {response.status_code}: {response_data}"
+    )
+
+
+async def throw_err_on_msg(msg, text=""):
+    await msg.add_reaction(emoji=EMOJI_RED_CROSS)
+
+    if text:
+        await msg.reply(text)
+
+    raise RuntimeError(
+        f"File could not be created on server: {response_data}"
+    )
+
+
+async def clear_all_reactions(channel):
+    async for msg in channel.history():
+        await msg.clear_reactions()
+
+
+def full_username(user):
+    return f"{user.display_name}#{user.discriminator}"
 
 
 def build_filename(s):
--- src/bot.py
+++ src/bot.py
@@ -1,39 +1,62 @@
 import os
+from collections import namedtuple
+from contextlib import suppress
 from random import seed
 from typing import Optional
 
 import discord
-import requests
-from discord import Emoji
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from src.actions import handle_existing_seed_messages
+from src.actions import (EMOJI_RED_CROSS, handle_existing_seed_messages,
+                         handle_message)
 
 load_dotenv()
 
 
-API_AUTH_SECRET = os.getenv('API_AUTH_SECRET')
-
 BOT_TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD_NAME = os.getenv('DISCORD_GUILD')
 
-GUILD: Optional[discord.Guild] = None
-SEEDS_CHANNEL: Optional[discord.TextChannel] = None
+GUILD_NAME = "pingu's arctic funhouse"
+SEEDS_CHANNEL_NAME = "tt2-raid-rolls"
+
+CommandArgument = namedtuple(
+    "CommandArgument", ["name", "description", "type", "optional"],
+    defaults=["", "", "Any", False]
+)
 
 COMMAND_PREFIX = '!'
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
+def get_guild_seeds_channel():
+    guild = discord.utils.get(bot.guilds, name=GUILD_NAME)
+    seeds_channel = discord.utils.get(
+        guild.text_channels, name=SEEDS_CHANNEL_NAME
+    )
+
+    return guild, seeds_channel
+
+
+def validate_context(context, guild, target_channels) -> bool:
+    if context.guild != guild:
+        raise commands.CommandError(
+            "This command does not work in this guild"
+        )
+
+    if context.channel not in target_channels:
+        raise commands.CommandError(
+            "This command does not work in this channel"
+        )
+
+    return True
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}#{bot.user.discriminator} has connected to Discord!')
 
-    guild = discord.utils.get(bot.guilds, name=GUILD_NAME)
-    seeds_channel = discord.utils.get(
-        guild.text_channels, name="tt2-raid-rolls"
-    )
+    guild, seeds_channel = get_guild_seeds_channel()
 
     if not guild or not seeds_channel:
         print("Did not connect to guild or text channels, closing bot client.")
@@ -42,15 +65,14 @@ async def on_ready():
 
     print(f'{bot.user.name}#{bot.user.discriminator} has connected to {GUILD_NAME}.#{seeds_channel.name}')
 
-    global GUILD, SEEDS_CHANNEL
-    GUILD = guild
-    SEEDS_CHANNEL = seeds_channel
-
     print("checking existing messages")
 
-    await handle_existing_seed_messages(seeds_channel)
+    try:
+        await handle_existing_seed_messages(seeds_channel)
+    except RuntimeError as e:
+        await seeds_channel.send(e)
 
-    await bot.close()
+    print("done | bot is listening")
 
 
 @bot.event
@@ -61,7 +83,56 @@ async def on_message(message):
     if isinstance(message.channel, discord.DMChannel):
         await message.channel.send(content="I don't read my DMs, I'm really busy")
 
-    print(message)
+    await bot.process_commands(message)
+
+    with suppress(RuntimeError):
+        await handle_message(message)
+
+
+@bot.command(
+    name='delete-messages', aliases=['del'],
+)
+@commands.has_role('admin')
+async def delete_recent_messages(context, count: int = 1):
+    guild, seeds_channel = get_guild_seeds_channel()
+
+    if not validate_context(context, guild, (seeds_channel, )):
+        return
+
+    if count <= 0:
+        raise commands.BadArgument("Argument [count] must be greater than 0")
+
+    await context.message.delete()
+
+    messages = await context.channel.history(limit=count).flatten()
+
+    for message in messages:
+        await message.delete()
+
+    print(f"Deleted {count} messages in #{context.channel.name}")
+
+
+@bot.command(
+    name='clear-reactions', aliases=['cr'],
+)
+@commands.has_role('admin')
+async def clear_reactions(context, count: int = 1):
+    guild, seeds_channel = get_guild_seeds_channel()
+
+    if not validate_context(context, guild, (seeds_channel, )):
+        return
+
+    if count <= 0:
+        raise commands.BadArgument("Argument [count] must be greater than 0")
+
+    await context.message.delete()
+
+    messages = await context.channel.history(limit=count).flatten()
+
+    for message in messages:
+        await message.clear_reactions()
+
+    print(f"Cleared reactions on {count} messages in #{context.channel.name}")
 
 
 def main():
