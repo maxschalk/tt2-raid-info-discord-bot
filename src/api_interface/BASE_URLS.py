2062369e5a0777eff9be519c703a4ed1b7276781
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-17 15:39:13 +0200

    remove STAGING

--- src/api_interface/BASE_URLS.py
+++ src/api_interface/BASE_URLS.py
@@ -2,6 +2,5 @@ from src.api_interface.Stage import Stage
 
 BASE_URLS = {
     Stage.DEV: "http://localhost:5000/api/v0",
-    Stage.STAGING: "https://tt2-data-staging.herokuapp.com/api/v0",
     Stage.PRODUCTION: "https://tt2-data.herokuapp.com/api/v0",
 }
