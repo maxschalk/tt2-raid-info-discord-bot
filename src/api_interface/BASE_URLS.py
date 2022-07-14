7b669d5de34635af15954df5187beb1652e7509c
riskypenguin <maximilian.schalk@audi.de>
2022-07-14 21:00:16 +0200

    update api prod url

--- src/api_interface/BASE_URLS.py
+++ src/api_interface/BASE_URLS.py
@@ -2,5 +2,5 @@ from src.api_interface.Stage import Stage
 
 BASE_URLS_API = {
     Stage.DEV: "http://localhost:5000/api/v0",
-    Stage.PRODUCTION: "http://riskypengu.in/api/v0",
+    Stage.PRODUCTION: "https://tt2-raid-info-api.up.railway.app/api/v0",
 }
