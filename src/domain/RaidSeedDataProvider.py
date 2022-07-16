b9decc117a703e7a77e60a935bd711a0b5838100
riskypenguin <maximilian-schalk@web.de>
2022-07-16 20:20:39 +0200

    refactor: add RaidSeedDataProvider

---
+++ src/domain/RaidSeedDataProvider.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+from src.model.SeedType import SeedType
+
+
+class RaidSeedDataProvider(ABC):
+
+    @abstractmethod
+    def list_seed_identifiers(self: RaidSeedDataProvider,
+                              *,
+                              seed_type: SeedType = SeedType.RAW,
+                              count: int = None) -> list[str]:
+        pass
+
+    @abstractmethod
+    def save_seed(self: RaidSeedDataProvider, *, identifier: str,
+                  data: list[Any]) -> bool:
+        pass
+
+    @abstractmethod
+    def get_seed(
+        self: RaidSeedDataProvider,
+        *,
+        identifier: str,
+        seed_type: SeedType = SeedType.RAW,
+    ) -> list[Any] | None:
+        pass
+
+    @abstractmethod
+    def delete_seed(
+        self: RaidSeedDataProvider,
+        *,
+        identifier: str,
+    ) -> bool:
+        pass
