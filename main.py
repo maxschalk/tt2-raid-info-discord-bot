ceace0dfe48cf8825b31e51aebb2e12060d834dc
riskypenguin <maximilian-schalk@web.de>
2022-07-17 17:14:55 +0200

    refactor: factory pattern & dependency injection

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from src.bot import bot
+from discord.ext import commands
+
+from src.bot.bot import setup_bot
+from src.raid_seed_data_api.raid_seed_data_api import RaidSeedDataAPI
+from src.utils import get_env
+
+GUILD_NAME = get_env("GUILD_NAME")
+SEEDS_CHANNEL_NAME = get_env("SEEDS_CHANNEL_NAME")
+
+BOT_TOKEN = get_env('DISCORD_TOKEN')
+
+DATA_PROVIDER = RaidSeedDataAPI(
+    base_url=get_env("RAID_SEED_DATA_API_BASE_URL"),
+    auth_key=get_env("RAID_SEED_DATA_API_AUTH_SECRET"))
 
 
 def main():
-    bot.main()
+    bot = commands.Bot(command_prefix='!')
+
+    setup_bot(bot, GUILD_NAME, SEEDS_CHANNEL_NAME, DATA_PROVIDER)
+
+    bot.run(BOT_TOKEN)
 
 
 if __name__ == '__main__':
