2143e24a82723e719919700b547ab4b0816f1028
riskypenguin <maximilian-schalk@web.de>
2022-07-17 17:14:55 +0200

    refactor: factory pattern & dependency injection

--- src/bot/actions.py
+++ src/bot/actions.py
@@ -1,20 +1,15 @@
 import io
 import json
 from contextlib import suppress
-from http.client import HTTPException
 
 import discord
 import requests
 from discord.ext import commands
+from src.domain.raid_seed_data_provider import RaidSeedDataProvider
 from src.model.seed_type import SeedType
-from src.raid_seed_data_api.raid_seed_data_api import RaidSeedDataAPI
-from src.utils import get_env
 
 from .utils import (BOT_AUTHOR, BOT_USER, EMOJI_CHECK_MARK, EMOJI_RED_CROSS,
                     full_username, is_relevant_message, seed_identifier)
-
-provider = RaidSeedDataAPI(base_url=get_env("RAID_SEED_DATA_API_BASE_URL"),
-                           auth_key=get_env("RAID_SEED_DATA_API_AUTH_SECRET"))
 
 
 async def _is_handled(reaction):
@@ -31,7 +26,7 @@ async def _is_handled(reaction):
     return False
 
 
-async def _throw_err_on_msg(msg, text=""):
+async def _throw_err_on_msg(msg, text=None):
     await msg.add_reaction(emoji=EMOJI_RED_CROSS)
 
     if text:
@@ -40,93 +35,6 @@ async def _throw_err_on_msg(msg, text=""):
     raise RuntimeError(
         f"Something went wrong, please check individual {EMOJI_RED_CROSS} message replies"
     )
-
-
-async def _clear_all_reactions(channel):
-    async for msg in channel.history():
-        await msg.clear_reactions()
-
-
-async def process_existing_messages(channel):
-    async for msg in channel.history():
-
-        for reaction in msg.reactions:
-            if await _is_handled(reaction):
-                return
-
-        if full_username(msg.author) == BOT_USER:
-            await msg.delete()
-
-        with suppress(RuntimeError):
-            await process_message(msg)
-
-
-async def process_message(msg):
-
-    if not is_relevant_message(msg):
-        return
-
-    print("relevant message found:", msg.content)
-
-    if len(msg.attachments) != 1:
-        await msg.add_reaction(emoji=EMOJI_RED_CROSS)
-
-        await _throw_err_on_msg(
-            msg, f"Message fits criteria (author, content format), \
-            but has {len(msg.attachments)} (!= 1) attachments")
-
-    attachment, *_ = msg.attachments
-
-    data = requests.get(attachment.url).json()
-
-    identifier = seed_identifier(from_msg_content=msg.content)
-
-    try:
-        provider.save_seed(identifier=identifier, data=json.dumps(data))
-    except HTTPException as error:
-        await _throw_err_on_msg(msg, f"Error saving seed: {error}")
-
-    await msg.add_reaction(emoji=EMOJI_CHECK_MARK)
-
-
-async def get_seed_identifiers(context, count: int = None) -> list[str] | str:
-
-    try:
-        data = provider.list_seed_identifiers()
-    except HTTPException as error:
-        await context.channel.send(f"Error getting seed identifiers: {error}")
-
-    count = count or len(data)
-
-    seed_ids = data[-count:]
-
-    text = str.join('\n', seed_ids)
-
-    await context.channel.send(f"_ _\n{text}")
-
-
-async def get_seed_data(context, identifier: str,
-                        seed_type: SeedType) -> tuple[str, str]:
-
-    try:
-        data = provider.get_seed(identifier=identifier, seed_type=seed_type)
-    except HTTPException as error:
-        await context.channel.send(f"Error getting seed: {error}")
-
-    file_obj = io.StringIO(json.dumps(data, indent=4))
-    await context.channel.send(file=discord.File(
-        fp=file_obj, filename=f"{seed_type.value}_{identifier}"))
-
-
-async def delete_seed(context, identifier: str):
-
-    try:
-        provider.delete_seed(identifier=identifier)
-    except HTTPException as error:
-        await context.channel.send(f"Error deleting seed: {error}")
-        return
-
-    await context.channel.send(f"Deleted seed {identifier}")
 
 
 async def delete_recent_messages(context, count: int = 1):
@@ -152,3 +60,91 @@ async def clear_reactions(context, count: int = 1):
         await message.clear_reactions()
 
     print(f"Cleared reactions on {count} messages in #{context.channel.name}")
+
+
+async def process_existing_messages(channel,
+                                    data_provider: RaidSeedDataProvider):
+    async for msg in channel.history():
+
+        for reaction in msg.reactions:
+            if await _is_handled(reaction):
+                return
+
+        if full_username(msg.author) == BOT_USER:
+            await msg.delete()
+
+        with suppress(RuntimeError):
+            await process_message(msg, data_provider)
+
+
+async def process_message(msg, data_provider: RaidSeedDataProvider):
+
+    if not is_relevant_message(msg):
+        return
+
+    print("relevant message found:", msg.content)
+
+    if len(msg.attachments) != 1:
+        await msg.add_reaction(emoji=EMOJI_RED_CROSS)
+
+        await _throw_err_on_msg(
+            msg, f"Message fits criteria (author, content format), \
+            but has {len(msg.attachments)} (!= 1) attachments")
+
+    attachment = msg.attachments[0]
+
+    data = requests.get(attachment.url).json()
+
+    identifier = seed_identifier(from_msg_content=msg.content)
+
+    try:
+        data_provider.save_seed(identifier=identifier, data=json.dumps(data))
+    except Exception as error:
+        await _throw_err_on_msg(msg, f"Error saving seed: {error}")
+
+    await msg.add_reaction(emoji=EMOJI_CHECK_MARK)
+
+
+async def get_seed_identifiers(context,
+                               data_provider: RaidSeedDataProvider,
+                               count: int = None) -> list[str] | str:
+
+    try:
+        data = data_provider.list_seed_identifiers()
+    except Exception as error:
+        await context.channel.send(f"Error getting seed identifiers: {error}")
+
+    count = count or len(data)
+
+    seed_ids = data[-count:]
+
+    text = str.join('\n', seed_ids)
+
+    await context.channel.send(f"_ _\n{text}")
+
+
+async def get_seed_data(context, data_provider: RaidSeedDataProvider,
+                        identifier: str,
+                        seed_type: SeedType) -> tuple[str, str]:
+
+    try:
+        data = data_provider.get_seed(identifier=identifier,
+                                      seed_type=seed_type)
+    except Exception as error:
+        await context.channel.send(f"Error getting seed: {error}")
+
+    file_obj = io.StringIO(json.dumps(data, indent=4))
+    await context.channel.send(file=discord.File(
+        fp=file_obj, filename=f"{seed_type.value}_{identifier}.json"))
+
+
+async def delete_seed(context, data_provider: RaidSeedDataProvider,
+                      identifier: str):
+
+    try:
+        data_provider.delete_seed(identifier=identifier)
+    except Exception as error:
+        await context.channel.send(f"Error deleting seed: {error}")
+        return
+
+    await context.channel.send(f"Deleted seed {identifier}")
--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,29 +1,52 @@
 from contextlib import suppress
+from typing import Callable
 
 import discord
 from discord.ext import commands
+from src.domain.raid_seed_data_provider import RaidSeedDataProvider
 from src.model.seed_type import SeedType
-from src.utils import get_env
 
 from . import actions
 from .utils import full_username
 
-GUILD_NAME = get_env("GUILD_NAME")
-SEEDS_CHANNEL_NAME = get_env("SEEDS_CHANNEL_NAME")
 
-bot = commands.Bot(command_prefix='!')
+def setup_bot(bot: commands.Bot, guild_name: str, channel_name: str,
+              data_provider: RaidSeedDataProvider) -> None:
+
+    get_guild = _create_get_guild(bot, guild_name)
+    get_channel = _create_get_channel(channel_name)
+    process_context = _create_process_context(get_guild, get_channel)
+
+    # Event listeners
+    bot.add_listener(
+        _create_on_ready(bot, get_guild, get_channel, data_provider))
+    bot.add_listener(_create_on_message(bot, data_provider))
+
+    # Meta commands
+    bot.add_command(_create_delete_recent_messages(process_context))
+    bot.add_command(_create_clear_reactions(process_context))
+
+    # Raid seed data commands
+    bot.add_command(_create_process_existing(process_context, data_provider))
+    bot.add_command(
+        _create_get_seed_identifiers(process_context, data_provider))
+    bot.add_command(_create_get_seed_data(process_context, data_provider))
+    bot.add_command(_create_delete_seed(process_context, data_provider))
 
 
-def _get_guild_channel():
-    guild = discord.utils.get(bot.guilds, name=GUILD_NAME)
+def _create_get_guild(bot, guild_name) -> Callable:
+    return lambda: discord.utils.get(bot.guilds, name=guild_name)
 
-    if not guild:
-        return None, None
 
-    seeds_channel = discord.utils.get(guild.text_channels,
-                                      name=SEEDS_CHANNEL_NAME)
+def _create_get_channel(channel_name: str) -> Callable:
 
-    return guild, seeds_channel
+    def func(guild):
+        if not guild:
+            return None
+
+        return discord.utils.get(guild.text_channels, name=channel_name)
+
+    return func
 
 
 def _validate_context(context, guild, target_channels) -> bool:
@@ -37,104 +60,138 @@ def _validate_context(context, guild, target_channels) -> bool:
     return True
 
 
-@bot.event
-async def on_ready():
-    print(f'{full_username(bot.user)} has connected to Discord!')
+def _create_process_context(get_guild, get_channel):
 
-    guild, seeds_channel = _get_guild_channel()
+    async def process_context(context):
+        guild = get_guild()
+        seeds_channel = get_channel(guild)
 
-    if not guild or not seeds_channel:
-        print("Did not connect to guild or text channels, closing bot client.")
-        await bot.close()
-        return
+        if not _validate_context(context, guild, (seeds_channel, )):
+            return
 
-    print(
-        f'{full_username(bot.user)} has connected to {GUILD_NAME}.#{seeds_channel.name}'
-    )
+        await context.message.delete()
 
-    with suppress(RuntimeError):
-        await actions.process_existing_messages(seeds_channel)
+    return process_context
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+def _create_on_ready(bot, get_guild, get_channel, data_provider):
 
-    if isinstance(message.channel, discord.DMChannel):
-        await message.channel.send(
-            content="I don't read my DMs, I'm really busy")
+    async def on_ready():
+        print(f'{full_username(bot.user)} has connected to Discord!')
 
-    await bot.process_commands(message)
+        guild = get_guild()
+        seeds_channel = get_channel(guild)
 
-    await actions.process_message(message)
+        if not guild or not seeds_channel:
+            print(
+                "Did not connect to guild or text channels, closing bot client."
+            )
+            await bot.close()
+            return
 
+        print(
+            f'{full_username(bot.user)} has connected to {guild.name}.#{seeds_channel.name}'
+        )
 
-async def _process_context(context):
-    guild, seeds_channel = _get_guild_channel()
+        with suppress(RuntimeError):
+            await actions.process_existing_messages(seeds_channel,
+                                                    data_provider)
 
-    if not _validate_context(context, guild, (seeds_channel, )):
-        return
-
-    await context.message.delete()
-
-
-@bot.command(name='process', aliases=['p'])
-@commands.has_role('admin')
-async def process_existing(context):
-    await _process_context(context)
-
-    await actions.process_existing_messages(context.channel)
+    return on_ready
 
 
-@bot.command(name='seed-identifiers', aliases=['sids'])
-@commands.has_role('admin')
-async def get_seed_identifiers(context, count: int = None):
-    await _process_context(context)
+def _create_on_message(bot, data_provider):
 
-    await actions.get_seed_identifiers(context=context, count=count)
+    async def on_message(message):
+        if message.author == bot.user:
+            return
 
+        if isinstance(message.channel, discord.DMChannel):
+            await message.channel.send(
+                content="I don't read my DMs, I'm really busy")
 
-@bot.command(name='seed', aliases=['s'])
-@commands.has_role('admin')
-async def get_seed_data(context,
-                        identifier: str,
-                        seed_type: SeedType = SeedType.RAW):
-    await _process_context(context)
+            return
 
-    await actions.get_seed_data(context=context,
-                                identifier=identifier,
-                                seed_type=seed_type)
+        await actions.process_message(message, data_provider)
+
+    return on_message
 
 
-@bot.command(name='delete-seed', aliases=['ds'])
-@commands.has_role('admin')
-async def delete_seed(context, identifier: str):
-    await _process_context(context)
+def _create_delete_recent_messages(process_context):
 
-    await actions.delete_seed(context=context, identifier=identifier)
+    @commands.has_role('admin')
+    @commands.command(name='delete-messages', aliases=['del'])
+    async def func(context, count: int = 1):
+        await process_context(context)
 
+        await actions.delete_recent_messages(context=context, count=count)
 
-@bot.command(name='delete-messages', aliases=['del'])
-@commands.has_role('admin')
-async def delete_recent_messages(context, count: int = 1):
-    await _process_context(context)
-
-    await actions.delete_recent_messages(context=context, count=count)
+    return func
 
 
-@bot.command(name='clear-reactions', aliases=['cr'])
-@commands.has_role('admin')
-async def clear_reactions(context, count: int = 1):
-    await _process_context(context)
+def _create_clear_reactions(process_context):
 
-    await actions.clear_reactions(context=context, count=count)
+    @commands.has_role('admin')
+    @commands.command(name='clear-reactions', aliases=['cr'])
+    async def func(context, count: int = 1):
+
+        await process_context(context)
+
+        await actions.clear_reactions(context=context, count=count)
+
+    return func
 
 
-def main():
-    token = get_env('DISCORD_TOKEN')
-    bot.run(token)
+def _create_process_existing(process_context, data_provider):
+
+    @commands.has_role('admin')
+    @commands.command(name='process', aliases=['p'])
+    async def func(context):
+        await process_context(context)
+
+        await actions.process_existing_messages(context.channel, data_provider)
+
+    return func
 
 
-if __name__ == '__main__':
-    main()
+def _create_get_seed_identifiers(process_context, data_provider):
+
+    @commands.has_role('admin')
+    @commands.command(name='seed-identifiers', aliases=['sids'])
+    async def func(context, count: int = None):
+        await process_context(context)
+
+        await actions.get_seed_identifiers(context=context,
+                                           data_provider=data_provider,
+                                           count=count)
+
+    return func
+
+
+def _create_get_seed_data(process_context, data_provider):
+
+    @commands.has_role('admin')
+    @commands.command(name='seed', aliases=['s'])
+    async def func(context,
+                   identifier: str,
+                   seed_type: SeedType = SeedType.RAW):
+        await process_context(context)
+
+        await actions.get_seed_data(context=context,
+                                    data_provider=data_provider,
+                                    identifier=identifier,
+                                    seed_type=seed_type)
+
+    return func
+
+
+def _create_delete_seed(process_context, data_provider):
+
+    @commands.has_role('admin')
+    @commands.command(name='delete-seed', aliases=['ds'])
+    async def func(context, identifier: str):
+        await process_context(context)
+
+        await actions.delete_seed(context, data_provider, identifier)
+
+    return func
