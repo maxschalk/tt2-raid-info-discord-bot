17b4f39d2ae51f4c4682976e77cfe0f1f2eb77d9
riskypenguin <maximilian-schalk@web.de>
2022-07-17 14:44:44 +0200

    flush command print before execution

--- scripts/run_script.py
+++ scripts/run_script.py
@@ -7,5 +7,5 @@ def run_script(get_cmds: Callable):
     files = sys.argv[1:]
 
     for cmd in get_cmds(files):
-        print(str.join(' ', cmd))
+        print(str.join(' ', cmd), flush=True)
         subprocess.run(cmd, check=True)
