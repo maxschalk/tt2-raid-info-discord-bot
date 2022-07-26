75e6fe53d3d3a2f9b932a08b21c9c8d6e1c8fc93
riskypenguin <maximilian-schalk@web.de>
2022-07-26 23:27:46 +0200

    improve pylint ignore rules

--- scripts/lint.py
+++ scripts/lint.py
@@ -6,8 +6,7 @@ from ._run_script import run_script
 
 def get_cmds(*, paths):
     if not paths:
-        return (("pylint", ".", "--recursive", "y", "--ignore-patterns",
-                 "venv"), )
+        return (("pylint", ".", "--recursive", "y"), )
 
     return (("pylint", path) for path in paths)
 
